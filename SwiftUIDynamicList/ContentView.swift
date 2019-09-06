245900a447665ebc34bd4a233adc3891170d61ed
Alex Nagy <support@rebeloper.com>
2019-09-06 10:35:04 +0300

    Initial Commit

---
+++ SwiftUIDynamicList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDynamicList
+//
+//  Created by Agota Matyas on 06/09/2019.
+//  Copyright © 2019 Rebeloper. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
